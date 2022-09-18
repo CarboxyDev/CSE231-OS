221e478184f144a6bf95b476979798efe7e041f6
Arman <37100541+CarboxyDev@users.noreply.github.com>
2022-09-18 16:04:39 +0530

    Add new io.asm file with extern printf call

--- Assignments/io.asm
+++ Assignments/io.asm
@@ -20,62 +20,16 @@ section .bss
 
 section .text
     global _start
+    extern printf
+    extern scanf
 
 _start:
-    ;call _printPromptNumber
-    ;call _inputNumber
-    call _printPromptString
-    call _inputString
-    ;call _printOutputNumber
-    ;call _printNumber
-    call _printOutputString
-    call _printString
-
-    ;mov rax, 1
-    ;mov rdi, 1
-    ;mov rsi, text
-    ;mov rdx, 14
-    ;syscall
-
-    mov rax, 60
-    mov rdi, 0
-    syscall
-
-
-_inputString:
-    mov rax, 0
-    mov rdi, 0 ; 0 -> STDIN
-    mov rsi, string
-    mov rdx, 64
-    syscall
+    push rbp
+    mov rbp, rsp
+    lea rdi, [promptNumber]
+    xor eax, eax
+    call printf
+    xor eax, eax
+    pop rbp
     ret
-
-
-
-_printPromptString:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, promptString
-    mov rdx, 13
-    syscall 
-    ret
-
-
-_printOutputString:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, outputString
-    mov rdx, 19
-    syscall 
-    ret
-
-
-_printString:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, string
-    mov rdx, 64
-    syscall 
-    ret
-
 
