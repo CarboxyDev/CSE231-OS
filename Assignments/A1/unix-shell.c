fda206543555a31cda251996c8c3cbabe5db24bc
Arman <37100541+CarboxyDev@users.noreply.github.com>
2022-10-11 03:18:56 +0530

    Implement new shell with significant code changes

--- Assignments/A1/unix-shell.c
+++ Assignments/A1/unix-shell.c
@@ -3,56 +3,79 @@
 #include <string.h>
 #include <unistd.h>
 
-#define clear() printf("\033[H\033[J")
-
 // Internal commands -> cd echo pwd 
 // External commands -> ls cat date rm mkdir
 
-void echoCommand(char** allArgs);
-int getWordCount(char* line);
 
-
-void executeLine(char* line) {
-	if (strcmp(line, "clear") == 0)
-	{
-		clear();
+void checkForBasicCommand(char* command) {
+	if (strcmp(command, "clear") == 0) {
+		printf("\033[H\033[J"); // clears the console
 	}
-	else if (strcmp(line, "exit") == 0)
-	{
+	else if (strcmp(command, "exit") == 0) {
 		printf("\n");
 		exit(0);
 	};
 }
 
-void parseShellLine(char* line) {
+void shellPrompt() {
+	char* user = getenv("USER");
+	printf("%s $ ", user);
+}
 
-
-
-	executeLine(line);
-};
-
-
-void shell() {
-	const char USERNAME[] = "arman";
+void shellInput(char command[], char* args[]) {
 	char line[1000];
+	char* arr[100];
+	char* parse;
 
-	printf("%s $ ", USERNAME);
+	int c = 0;
+	while (1) {
+		int chr = getc(stdin);
+		line[c++] = (char) chr;
+		if (chr == '\n') {
+			break;
+		}
+	};
+
+	if (c == 1) { return; }; // no command was written
+	parse = strtok(line, "\n"); // remove the trailing \n at the end of the line. This solution might not be thread-safe
+
+	int c2 = 0;
+	while (parse != NULL) { // turn the line into -> words
+		arr[c2++] = strdup(parse);
+		parse = strtok(NULL, "\n");
+	} 
+
+	strcpy(command, arr[0]);
+ 
+	for (int x=0; x < c2; x++) {
+		args[x] = arr[x];
+	}
+
+	args[c2] = NULL; // end the arguments list with a NULL terminator
+	printf("%s\n", line);
+
+	/*
 	fgets(line, 1000, stdin);
 	strtok(line, "\n"); // remove the trailing \n at the end of the line. This solution might not be thread-safe
-
-	parseShellLine(line);
-
+ 	*/
+	//checkForBasicCommand(line); // run commands like exit, clear
 
 };
 
 int main()
 {
-
 	printf("[!] Switched to UNIX Shell.\n\n");
+
 	while (1)
 	{
-		shell();
-		
+		char command[100];
+		char rootCommand[100]; // basically the first word of the command like echo, ls, etc
+		char *args[100];
+
+		shellPrompt(); // prompt for the shell
+		shellInput(command, args); // gets the input for the shell
+
+		checkForBasicCommand(command); // check and run commands like exit, clear
 	}
 
 	return 0;
