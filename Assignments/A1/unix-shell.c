b712aad85870d5c097305d1c1a5977dc17db7cea
Arman <37100541+CarboxyDev@users.noreply.github.com>
2022-10-11 02:21:34 +0530

    Add changes before heavy changes to unix-shell.c

--- Assignments/A1/unix-shell.c
+++ Assignments/A1/unix-shell.c
@@ -1,57 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define clear() printf("\033[H\033[J")
 
 // Internal commands -> cd echo pwd 
 // External commands -> ls cat date rm mkdir
 
-int getWordCount(const char* line) {
-	int wordCount = 1;
-	for (int i = 0; i < strlen(line); i++) {
-		if (line[i] == 32 && i != 0) {
-			wordCount++;
-		}
-	}
-	return wordCount;
-}
+void echoCommand(char** allArgs);
+int getWordCount(char* line);
 
 
-
-void parseShellLine(char* line) {
-	if (strcmp(line, "clear") == 0) {
+void executeLine(char* line) {
+	if (strcmp(line, "clear") == 0)
+	{
 		clear();
 	}
-	else if (strcmp(line, "exit") == 0) {
+	else if (strcmp(line, "exit") == 0)
+	{
 		printf("\n");
 		exit(0);
 	};
-
-
-	if (getWordCount(line) > 1) {
-		char* wordArg;
-		char* allArgs[getWordCount(line)];
-		wordArg = strtok(line, " ");
-		int i = 0;
-		while (wordArg != NULL) {
-			printf(" -> %s", wordArg);
-			wordArg = strtok(NULL, " ");
-			strcpy(allArgs[i], wordArg);
-			i++;
-		}
-		printf("\n");
-	};
-
 }
 
+void parseShellLine(char* line) {
+
+
+
+	executeLine(line);
+};
 
 
 void shell() {
 	const char USERNAME[] = "arman";
 	char line[1000];
 
-	
 	printf("%s $ ", USERNAME);
 	fgets(line, 1000, stdin);
 	strtok(line, "\n"); // remove the trailing \n at the end of the line. This solution might not be thread-safe
@@ -61,17 +45,34 @@ void shell() {
 
 };
 
+int main()
+{
 
-
-
-
-int main() {
-	
 	printf("[!] Switched to UNIX Shell.\n\n");
-	while (1) {
+	while (1)
+	{
 		shell();
+		
 	}
-	
-	
+
 	return 0;
+}
+
+
+void echoCommand(char **allArgs)
+{
+	printf("echo command found. First arg is %s", allArgs[0]);
+};
+
+int getWordCount(char *line)
+{
+	int wordCount = 1;
+	for (int i = 0; i < strlen(line); i++)
+	{
+		if (line[i] == 32 && i != 0)
+		{
+			wordCount++;
+		}
+	}
+	return wordCount;
 }
