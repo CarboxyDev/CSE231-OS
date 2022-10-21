0b0c1b4d1421077f3883229041f65a025bd43b1e
Arman <37100541+CarboxyDev@users.noreply.github.com>
2022-10-22 01:08:08 +0530

    Add cat command for single file

--- Assignments/A1/cmdbin/cat.c
+++ Assignments/A1/cmdbin/cat.c
@@ -11,8 +11,32 @@
 
 
 int main(int argc, char* argv[]) {
-    //printf("Execute cat");
-    //printf("No. of arguments -> %d\n",argc);
+
+    if (argc == 0) { // Not sure if this is the correct way to handle this case as the mac terminal just seems to hang whenever cat is called with no args
+        printf("Error: You need to give some file(s) as argument\n");
+    }
+    else if (argc == 1) { // Handle a single file
+        FILE *file;
+        file = fopen(argv[0],"r");
+
+        if (file == NULL) {
+            printf("cat: Error: Invalid file name provided\n");
+        }
+        else {
+            while (1) {
+                char chr = fgetc(file);
+                if (chr == EOF) {
+                    break;
+                }
+                printf("%c", chr);
+            }
+        }
+
+        printf("\n");
+    }
+    
+
+
 
     return 0;
 }
