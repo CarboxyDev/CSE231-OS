f3baa1f3bf0c329a2baa24dc46d9684ab638945f
Arman <37100541+CarboxyDev@users.noreply.github.com>
2022-10-24 18:09:34 +0530

    Add edgecase for cat command

--- Assignments/A1/cmdbin/cat.c
+++ Assignments/A1/cmdbin/cat.c
@@ -3,40 +3,56 @@
  *  The created binary will allow the shell to use the cat command (following the POSIX standard while missing many common features)
  *  The cat command is used to view the contents of a file (or multiple files, concatenated) specified by the user
  *  Supported Flags: -b, -n
- *  Supported Edge cases: TBA
+ *  Supported Edge cases: 1. Disallow using cat on folders  2. TBA
  */
 
 
 #include <stdio.h>
+#include <sys/stat.h>
+
+
+// check if the file is a directory or not.
+int checkFile(char filePath[]) {
+    struct stat file;
+    stat(filePath, &file);
+    return S_ISREG(file.st_mode); // returns 0 if given path is a directory and not a file
+} 
+
 
 
 int main(int argc, char* argv[]) {
-
     if (argc == 0) { // Not sure if this is the correct way to handle this case as the mac terminal just seems to hang whenever cat is called with no args
         printf("Error: You need to give some file(s) as argument\n");
     }
     else if (argc == 1) { // Handle a single file
-        FILE *file;
-        file = fopen(argv[0],"r");
-
-        if (file == NULL) {
-            printf("cat: Error: Invalid file name provided\n");
+        
+        if (!checkFile(argv[0])) {
+            printf("cat: %s is a directory\n", argv[0]);
         }
         else {
-            while (1) {
-                char chr = fgetc(file);
-                if (chr == EOF) {
-                    break;
-                }
-                printf("%c", chr);
+            FILE *file;
+            file = fopen(argv[0], "r");
+
+            if (file == NULL) {
+                printf("cat: Error: Invalid file name provided\n");
             }
+            else
+            {
+                while (1)
+                {
+                    char chr = fgetc(file);
+                    if (chr == EOF)
+                    {
+                        break;
+                    }
+                    printf("%c", chr);
+                }
+            }
+
+            printf("\n");
         }
 
-        printf("\n");
     }
     
-
-
-
     return 0;
 }
