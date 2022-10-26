2fe7762b67389d2ee83165d87ee21b19cf0bc6cf
Arman <37100541+CarboxyDev@users.noreply.github.com>
2022-10-26 15:47:50 +0530

    Finish ls command

--- Assignments/A1/cmdbin/ls.c
+++ Assignments/A1/cmdbin/ls.c
@@ -4,10 +4,10 @@
  *  The ls command is primarily used to view the contents of the shell's current working directory
  *  Supported Flags: -a, -l
  *  Supported Edge cases:
- *      1. Allow usage of ls on directories relative to the current directory
- *      2. Handle the case where the ls'd directory does not exist
+ *      1. Allow usage of ls on directories relative to the current working directory
+ *      2. Handle the case where the ls'd directory does not exist (supporting edge case 1)
  * 
- *  TODO: Implement the edge cases
+ *  TODO: None
  */
 
 
@@ -27,7 +27,7 @@ int main(int argc, char* argv[]) {
             struct dirent *dir;
             DIR *dirNav = opendir(currentDir);
             if (dirNav == 0) {
-                printf("Error in running ls on this directory");
+                printf("ls: Unable to view this directory\n");
                 return 0;
             }
             
@@ -59,6 +59,36 @@ int main(int argc, char* argv[]) {
             printf("\n");
 
         }
+    }
+    else if (argc == 1) { // argv[0] contains the directory name relative to the cwd
+        char currentDir[PATH_MAX];
+        if (getcwd(currentDir, sizeof(currentDir)) != NULL) {
+            struct dirent *dir;
+            strcat(currentDir, "/");
+            strcat(currentDir, argv[0]);
+
+            DIR *dirNav = opendir(currentDir);
+            if (dirNav == 0) {
+                printf("ls: %s: No such directory exists\n", argv[0]);
+                return 0;
+            }
+
+            dir = readdir(dirNav);
+            while (dir != NULL) {
+                if (dir->d_name[0] == '.') {
+                    dir = readdir(dirNav);
+                    continue; // skip the dotfiles/hidden
+                }
+                printf("%s   ", dir->d_name); // print the dir name
+
+                dir = readdir(dirNav);
+            }
+            
+            printf("\n");
+        }
+    }
+    else {
+        printf("ls: Invalid arguments given\n");
     }
 
     return 0;
